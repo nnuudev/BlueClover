285fb91a322eaa4a76e0e13b2bd2b3acb1074aea
nnuudev <84907027+nnuudev@users.noreply.github.com>
2022-12-07 14:58:28 +0100

    Removing unused legacy stuff.

--- Clover/app/src/main/java/org/floens/chan/utils/LocaleUtils.java
+++ Clover/app/src/main/java/org/floens/chan/utils/LocaleUtils.java
@@ -1,10 +1,8 @@
 package org.floens.chan.utils;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.os.Build;
 import android.webkit.WebView;
 
 import org.floens.chan.core.settings.ChanSettings;
@@ -13,13 +11,11 @@ import java.util.Locale;
 
 public class LocaleUtils {
     public static void overrideLocaleToEnglishIfNeeded(Context context) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 &&
-                ChanSettings.forceEnglishLocale.get()) {
+        if (ChanSettings.forceEnglishLocale.get()) {
             setLocaleToEnglish(context);
         }
     }
 
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     private static void setLocaleToEnglish(Context context) {
         // Android is so retarded holy shit
         // see https://stackoverflow.com/questions/40398528
