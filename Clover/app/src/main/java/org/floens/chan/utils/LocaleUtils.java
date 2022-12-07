5f81b0e3afe7f9715fe796d1c62eb161bfe2d45b
nnuudev <84907027+nnuudev@users.noreply.github.com>
2022-12-07 14:57:28 +0100

    Fixing the force English locale thing.

--- Clover/app/src/main/java/org/floens/chan/utils/LocaleUtils.java
+++ Clover/app/src/main/java/org/floens/chan/utils/LocaleUtils.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
+import android.webkit.WebView;
 
 import org.floens.chan.core.settings.ChanSettings;
 
@@ -20,6 +21,10 @@ public class LocaleUtils {
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     private static void setLocaleToEnglish(Context context) {
+        // Android is so retarded holy shit
+        // see https://stackoverflow.com/questions/40398528
+        new WebView(context).destroy();
+
         Resources resources = context.getResources();
         Configuration configuration = resources.getConfiguration();
 
